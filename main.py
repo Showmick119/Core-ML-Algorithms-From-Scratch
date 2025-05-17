468cf381228e15b03fff83527bd9986cc1ee02f8
Showmick Das <showmickdas75@gmail.com>
2025-05-17 08:34:45 -0400

    Updates

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class LinearRegression:
-    def __init__(self, learning_rate=0.01, n_features=5):
-        self.learning_rate = learning_rate
-        self.n_features = n_features
-    
-    def predict(self) -> float:
-        x = 2
-    
-    def cost(self) -> float:
-        x = 2
-
-    def gradient_descent(self) -> float:
-        x = 2
+x = 2
