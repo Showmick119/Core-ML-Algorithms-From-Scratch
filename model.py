6c0543b8ba67a7e8854073ca5ec45c59197f8a08
Showmick Das <showmickdas75@gmail.com>
2025-05-19 18:05:59 -0400

    Updates to __init__( ) method

--- model.py
+++ model.py
@@ -5,10 +5,12 @@ class MyLinearRegression:
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.normalize = normalize
+        self.loss_history = []
+        self.parameter_history = []
         self.weights = None
         self.bias = None
 
-    def fit(self) -> None:
+    def fit(self, X, y) -> None:
         x = 2
 
     def predict(self, X: np.ndarray) -> np.ndarray:
@@ -23,7 +25,7 @@ class MyLinearRegression:
 
         return y_pred
 
-    def _compute_cost(self, X, y) -> float:
+    def _compute_cost(self, X: np.ndarray, y: np.ndarray) -> float:
         cost = 0
         y_pred = self.predict(X)
         m = y_pred.shape[0]
@@ -34,7 +36,10 @@ class MyLinearRegression:
 
         return cost
 
-    def gradient_descent(self) -> float:
+    def _compute_gradient(self, X: np.ndarray, y: np.ndarray) -> float:
+        x = 2
+
+    def _gradient_descent(self, X, y) -> float:
         x = 2
     
     def _normalize_features(self, X: np.ndarray) -> np.ndarray:
