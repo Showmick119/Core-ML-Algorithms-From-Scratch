933ef376df19668097047dbe2945917f2083fb4d
Showmick Das <showmickdas75@gmail.com>
2025-05-19 16:24:13 -0400

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,10 @@ class MyLinearRegression:
     def __init__(self, learning_rate=0.01, n_features=5):
         self.learning_rate = learning_rate
         self.n_features = n_features
-    
+
+    def fit(self) -> None:
+        x = 2
+
     def predict(self) -> float:
         x = 2
     
