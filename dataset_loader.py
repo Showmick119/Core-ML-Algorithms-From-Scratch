5973326aacccca75a4ded65eab1a6a7ca31272c3
Showmick Das <showmickdas75@gmail.com>
2025-05-19 13:49:38 -0400

    Updates to dataset_loader

---
+++ dataset_loader.py
@@ -0,0 +1,15 @@
+import numpy as np
+from typing import Dict, Any
+
+class Dataset:
+    def __init__(self, X: np.array) -> None:
+            self.X = X
+
+    def zscore_normalization(self, X: Dict[str, int]) -> np.ndarray:
+         x = 2
+
+    def mean_normalization(self):
+        x = 2
+
+    def standard_normalization(self):
+        x =2
