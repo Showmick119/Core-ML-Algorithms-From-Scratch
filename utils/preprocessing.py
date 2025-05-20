1064d7eadb0374a66a03675eb1ac9f06d7f19888
Showmick Das <showmickdas75@gmail.com>
2025-05-19 23:22:20 -0400

    Initialize preprocessing.py

---
+++ utils/preprocessing.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def train_test_split(X, y, test_size=0.2, shuffle=True, random_state=None):
+    "return X_train, X_test, y_train, y_test"
+
+def standardize(X, mean=None, std=None):
+    "return X_scaled, mean, std"
